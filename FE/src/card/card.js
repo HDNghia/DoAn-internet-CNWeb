8e14f560c5289adc6c4121a6c02186e182babcc3
HDNghia <huynhdangnghia68@gmail.com>
2022-12-08 12:15:44 +0700

    nghia

--- FE/src/card/card.js
+++ FE/src/card/card.js
@@ -1,46 +1,48 @@
 // import 'regenerator-runtime/runtime';
 // import axios from 'axios';
+localStorage.setItem("login", true);
+
 for (var i = 0; i < localStorage.length; i++) {
-
     //add-card
-    var tr = document.createElement("tr");
-    var img = document.createElement("img");
-    var remove = document.createElement("input");
-    remove.setAttribute("type", "button")
-    remove.setAttribute("value", "Xóa")
-    remove.setAttribute("class", "btn btn-danger m-2 delete")
-    remove.setAttribute("onclick", `del(${i})`)
-    var x = localStorage.getItem(localStorage.key(i));
-    var anh = ".." + x.substring(x.indexOf("i") - 1, x.indexOf(","));
-    img.setAttribute("src", anh);
-    var item = [0, 1, 2, 3, 4, 5, 6];
-    item[0] = i;
-    item[1] = img;
-    item[2] = x.substring(x.indexOf(",") + 1, x.lastIndexOf(",") - 1)
-    item[3] = x.substring(x.lastIndexOf(",") + 1, x.length)
-    item[4] = 1
-    item[5] = x.substring(x.lastIndexOf(",") + 1, x.length)
-    item[6] = remove;
-    for (var j = 0; j < 7; j++) {
-        var td = document.createElement("td");
-        td.append(item[j]);
-        tr.append(td);
+    if (localStorage.key(i) != "login") {
+        var tr = document.createElement("tr");
+        var img = document.createElement("img");
+        var remove = document.createElement("input");
+        remove.setAttribute("type", "button")
+        remove.setAttribute("value", "Xóa")
+        remove.setAttribute("class", "btn btn-danger m-2 delete")
+        remove.setAttribute("onclick", `del(${i})`)
+        var x = localStorage.getItem(localStorage.key(i));
+        var anh = ".." + x.substring(x.indexOf("i") - 1, x.indexOf(","));
+        img.setAttribute("src", anh);
+        var item = [0, 1, 2, 3, 4, 5, 6];
+        item[0] = i;
+        item[1] = img;
+        item[2] = x.substring(x.indexOf(",") + 1, x.lastIndexOf(","))
+        item[3] = x.substring(x.lastIndexOf(",") + 1, x.length)
+        item[4] = 1
+        item[5] = x.substring(x.lastIndexOf(",") + 1, x.length)
+        item[6] = remove;
+        for (var j = 0; j < 7; j++) {
+            var td = document.createElement("td");
+            td.append(item[j]);
+            tr.append(td);
+        }
+        $(".order").append(tr);
+
+        function del(i) {
+            localStorage.removeItem(localStorage.key(i));
+            location.reload();
+        }
+
+        //delete card
+        function ClearAll() {
+            localStorage.clear();
+            location.reload();
+        }
     }
-    $(".order").append(tr);
-
-    function del(i) {
-        localStorage.removeItem(localStorage.key(i));
-        location.reload();
+    else {
     }
-
-    //delete card
-    function ClearAll() {
-        localStorage.clear();
-        location.reload();
-    }
-
-    //agree order
-
 }
 
 //get data
@@ -50,7 +52,11 @@ fetch('http://localhost:8080/api/v1/users')
     })
     .then(data => {
         console.log("check get data: ", data);
+        for (var i = 0; i < data.data.length; i++) {
+            console.log(data.data[i].id);
+        }
     })
+
 //post data
 async function postData(url = '', data = {}) {
     const response = await fetch(url, {
@@ -115,51 +121,74 @@ async function putData(url = '', data = {}) {
 
 
 
-
-
 $(".agreeOrder").click(function () {
-    var data = {
-        food: "",
-        price: "",
-        qty: "",
-        total: "",
-        order_date: "",
-        status: "",
-        customer_name: "",
-        customer_contact: "",
-        customer_email: "",
-        customer_address: ""
-    }
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0');
-    var yyyy = today.getFullYear();
-    today = mm + '/' + dd + '/' + yyyy;
-    data.customer_name = $(".name").val();
-    data.customer_address = $(".address").val();
-    data.customer_contact = $(".phone").val();
-    data.customer_email = $(".email").val();
-    var name_product = [];
-    var price_product = 0;
-    for (var i = 0; i < localStorage.length; i++) {
-        var x = localStorage.getItem(localStorage.key(i));
 
-        name_product = name_product + " " + x.substring(x.indexOf(",") + 1, x.lastIndexOf(",") - 1)
-        price_product = price_product + parseInt(x.substring(x.lastIndexOf(",") + 2, x.length))
-    }
-    data.food = name_product;
-    data.status = "delivery";
-    data.order_date = "today";
-    data.price = price_product;
-    data.qty = "1";
-    data.total = price_product;
-    console.log('check data from card: ', data)
-    console.log('check name product: ', name_product)
-    console.log('check price: ', price_product)
+    if (localStorage.getItem("login") == "true") {
 
-    postData('http://localhost:8080/api/v1/create-user', data)
-        .then(data => {
-            console.log(data);
-        })
+        var data = {
+            food: "",
+            price: "",
+            qty: "",
+            total: "",
+            order_date: "",
+            status: "",
+            customer_name: "",
+            customer_contact: "",
+            customer_email: "",
+            customer_address: ""
+        }
+
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0');
+        var yyyy = today.getFullYear();
+        today = mm + '/' + dd + '/' + yyyy;
+        data.customer_name = $(".name").val();
+        data.customer_address = $(".address").val();
+        data.customer_contact = $(".phone").val();
+        data.customer_email = $(".email").val();
+        var name_product = [];
+        var price_product = 0;
+        for (var i = 0; i < localStorage.length; i++) {
+            if (localStorage.key(i) == "login") { }
+            else {
+                var x = localStorage.getItem(localStorage.key(i));
+                name_product = name_product + " " + x.substring(x.indexOf(",") + 1, x.lastIndexOf(",") - 1)
+                price_product = price_product + parseInt(x.substring(x.lastIndexOf(",") + 2, x.length))
+            }
+        }
+        data.food = name_product;
+        data.status = "delivery";
+        data.order_date = today;
+        data.price = price_product;
+        data.qty = "1";
+        data.total = price_product;
+        console.log('check data from card: ', data)
+        console.log('check name product: ', name_product)
+        console.log('check price: ', price_product)
+        let isValid = true;
+        let arrInput = ['customer_name', 'customer_contact', 'customer_email', 'customer_address']
+        for (let i = 0; i < arrInput.length; i++) {
+            console.log('check inside loop', data[arrInput[i]], arrInput[i])
+            if (!data[arrInput[i]]) {
+                isValid = false;
+                alert('Missing parameter: ' + arrInput[i]);
+                return;
+            }
+        }
+
+        var div = document.createElement("div");
+        div.setAttribute("class", "alert alert-success");
+        div.append("order success")
+        $(".alert1").append(div);
+        postData('http://localhost:8080/api/v1/create-user', data)
+            .then(data => {
+                console.log(data);
+            })
+
+    }
+    else {
+        alert('mời bạn đăng nhập ')
+    }
 
 })
