d5dbef8f8fe3e4102114a6edc37061caa3068919
HDNghia <huynhdangnghia68@gmail.com>
2022-12-09 21:46:34 +0700

    update

--- BE/src/route/api.js
+++ BE/src/route/api.js
@@ -5,7 +5,6 @@ import APIController from '../controller/APIController';
 const initApiroute = (app) => {
     router.get('/users', APIController.getAllUser); //method GET => read data
     router.get('/users/:id', APIController.getDetailUser);
-    router.get('/users/customer_name/:customer_name', APIController.getUserForName);
     router.post('/create-user', APIController.createNewUser); //method POST => create data
     router.put('/update-user', APIController.updateUser); //method PUT => update data
     router.delete('/delete-user/:id', APIController.deleteUser); //method DELETE => DELETE data
