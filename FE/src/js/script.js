53b8b11f65944bbbf19d40186c3bf524d26271d6
HDNghia <huynhdangnghia68@gmail.com>
2022-12-10 11:55:02 +0700

    Update card

--- FE/src/js/script.js
+++ FE/src/js/script.js
@@ -1,3 +1,5 @@
+
+
 // swiping banners
 let banner = document.querySelector('.home__banner');
 let activeIndex = 0;
@@ -21,40 +23,6 @@ let looping = setInterval(next, 2000);
 //   loginForm.classList.remove('active');
 //   navbar.classList.remove('active');
 // }
-
-let shoppingCart = document.querySelector('.shopping-cart');
-
-document.querySelector('#cart-btn').onclick = () => {
-  shoppingCart.classList.toggle('active');
-  searchForm.classList.remove('active');
-  loginForm.classList.remove('active');
-  navbar.classList.remove('active');
-}
-
-let loginForm = document.querySelector('.login-form');
-
-document.querySelector('#login-btn').onclick = () => {
-  loginForm.classList.toggle('active');
-  searchForm.classList.remove('active');
-  shoppingCart.classList.remove('active');
-  navbar.classList.remove('active');
-}
-
-let navbar = document.querySelector('.navbar');
-
-document.querySelector('#menu-btn').onclick = () => {
-  navbar.classList.toggle('active');
-  searchForm.classList.remove('active');
-  shoppingCart.classList.remove('active');
-  loginForm.classList.remove('active');
-}
-
-window.onscroll = () => {
-  searchForm.classList.remove('active');
-  shoppingCart.classList.remove('active');
-  loginForm.classList.remove('active');
-  navbar.classList.remove('active');
-}
 
 var swiper = new Swiper(".product-slider", {
   loop: true,
@@ -97,6 +65,25 @@ var swiper = new Swiper(".review-slider", {
     },
   },
 });
+// let loginForm = document.querySelector('.login-form');
+
+// document.querySelector('#login-btn').onclick = () => {
+//   loginForm.classList.toggle('active');
+//   searchForm.classList.remove('active');
+//   shoppingCart.classList.remove('active');
+//   navbar.classList.remove('active');
+// }
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+  navbar.classList.remove('active');
+}
+
 
 function logout() {
   localStorage.setItem("loginUser", false);
